049008fe65f430e8c985ff1ea6035843e6972432
John Maunsell <maunsell@uchicago.edu>
2018-01-27 14:33:20 -0600

    Basler
    Created an objective C project that includes the CPP grab file.  Nothing working, just finally got it compiling.
    
    Signed-off-by: John Maunsell <maunsell@uchicago.edu>

---
+++ Basler Cameras/Basler/Basler/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  Basler
+//
+//  Created by John Maunsell on 1/27/18.
+//  Copyright © 2018 John Maunsell. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
