12a64ea1cbccab5914cab2d69956be7f3ba1928e
John Maunsell <maunsell@uchicago.edu>
2020-06-02 21:20:24 -0500

    Finished removing "Handles" from MT

--- 03 Saccade Metrics/MetricsAmpDur.m
+++ 03 Saccade Metrics/MetricsAmpDur.m
@@ -1,28 +1,17 @@
 classdef MetricsAmpDur < handle
-  %MetricsAmpDur Handle saccade-duration data for EOG
+  % MetricsAmpDur Handle saccade-duration data for EOG
   %   Accumulates and plots amplitude-duration relationships
   
   properties
     ampLabels
-%     offsetsDeg
-%     numOffsets
-%     fHandle
     lastN
     n
     reactTimesMS
-%     sampleRateHz
   end
   
   methods
     
-    function obj = MetricsAmpDur(app)
-      
-      %% Pre Initialization %%
-%       % Any code not using output argument (obj)
-%       if nargin == 0
-%         offsetList = 10;
-%       end
-      
+    function obj = MetricsAmpDur(app)      
       %% Object Initialization %%
       % Call superclass constructor before accessing object
       % You cannot conditionalize this statement
@@ -30,10 +19,6 @@ classdef MetricsAmpDur < handle
       obj = obj@handle();
       
       %% Post Initialization %%
-%       app.ampDurAxes = app.ampDurAxes;
-%       obj.sampleRateHz = sampleRate;
-%       obj.offsetsDeg = offsetList;
-%       obj.numOffsets = length(app.offsetsDeg);
       obj.reactTimesMS = zeros(10000, app.numOffsets);                  % preallocate a generous buffer
       obj.n = zeros(1, app.numOffsets);
       obj.lastN = 0;
@@ -57,7 +42,6 @@ classdef MetricsAmpDur < handle
       obj.lastN = 0;
       cla(app.ampDurAxes);
       axis(app.ampDurAxes, [0 1 0 1]);
-%       text(0.05, 0.1, '5', 'parent', app.ampDurAxes, 'FontSize', 24, 'FontWeight', 'Bold');
     end
     
     %% plotAmpDur
@@ -76,12 +60,7 @@ classdef MetricsAmpDur < handle
       medians = median(obj.reactTimesMS(1:minN, :));
       quartiles = prctile(obj.reactTimesMS(1:minN, :), [25 75]);
       [maxQ, indexMax] = max(max(quartiles));
-%       [minQ, indexMin] = min(min(quartiles));
-%       axis(app.ampDurAxes, [a(1), a(2), (medians(indexMin) - 2.0 * (medians(indexMin) - minQ))...
       axis(app.ampDurAxes, [a(1), a(2), 0, medians(indexMax) + 2.0 * (maxQ - medians(indexMax))]);
-%       a = axis(app.ampDurAxes);
-%       text(a(1) + 0.05 * (a(2) - a(1)), 0.1 * (a(4) - a(3)) + a(3), '5', 'parent', app.ampDurAxes, 'FontSize', 24,...
-%         'FontWeight', 'Bold');
       hold(app.ampDurAxes, 'off');
       obj.lastN = minN;
     end
